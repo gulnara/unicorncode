5a04b50bfdcdd97e23a54a6ec38386f93b4d86f0
Gulnara Mirzakarimova <gulnara@gulnara.me>
2013-05-04 18:28:19 -0700

    created database for user and tutorials

---
+++ model.py
@@ -0,0 +1,46 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, types
+from sqlalchemy import Column, Integer,Text, String, DateTime, Date
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+import datetime
+
+
+engine = create_engine("sqlite:///tutorials.db", echo=False)
+session = scoped_session(sessionmaker(bind=engine, autocommit = False, autoflush = False))
+
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+### Class declarations
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key = True)
+	email = Column(String(64), nullable = True)
+	password = Column(String(64), nullable = True)
+
+
+class Tutorial(Base):
+	__tablename__ = "tutorials"
+
+	id = Column(Integer, primary_key = True)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	title = Column(Text, nullable = True)
+	tutorial = Column(Text, nullable = True)
+
+	user = relationship("User", backref=backref("tutorials", order_by = id))
+	
+
+### End class declarations
+
+
+def main():
+    """In case we need this for something"""
+    pass
+
+if __name__ == "__main__":
+    main()
--- unicorncode.py
+++ unicorncode.py
@@ -2,36 +2,35 @@ import os
 from flask import Flask, render_template, redirect, url_for
 from flask import request
 from parser import parse
+import model
+from model import session as db_session, User, Tutorial
+
 
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-	return render_template("home.html")
+# @app.route("/home")
+# def home():
+# 	return render_template("home.html")
 
 @app.route("/")
-# def index():
-# 	return "this is unicorncode"
-
-def hello():
+@app.route("/index")
+def index():
 	return render_template("home.html")
-    # return """
-    # <html>
-
-    # <div id="main">
-    #     Type some code.  Press ctrl+enter to run it.<hr>
-    #     <textarea id="repl" name="code" rows="25" cols="80" ></textarea>
-    # </div>
-    # </html>
-
-
-    # """
 
 @app.route('/parse')
 def route():
     return '<pre>' + parse(request.args.get('lines', '')) + '</pre>'
 
+@app.route('/admin')
+def login():
+    return render_template("login.html")
 
+@app.route("/tutorials")
+def list_tutorials():
+
+	tutorials = db_session.query(Tutorial).all()
+	return render_template("list_tutorials.html", tutorials=tutorials)
+   
 if __name__ == "__main__":
 	app.run(debug=True)
