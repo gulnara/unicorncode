dbbdbf587121222f97f6c5f448c5ee1d9293a2e2
Gulnara Mirzakarimova <gulnara@gulnara.me>
2013-05-05 18:50:16 -0700

    added markdown

--- forms.py
+++ forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, BooleanField, TextField, TextAreaField, validators
 
 class TutorialForm(Form):
 	title = TextField('Title', validators=[validators.Required()])
-	text = TextAreaField('Tutorial', validators=[validators.Required()])
+	tutorial = TextAreaField('Tutorial', validators=[validators.Required()])
 
--- unicorncode.py
+++ unicorncode.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, flash, g
 from flask import request
 from parser import parse
 import model
+import markdown
 from model import session as db_session, User, Tutorial
 from forms import TutorialForm
 
@@ -16,6 +17,11 @@ app = Flask(__name__)
 @app.teardown_request
 def shutdown_session(exception = None):
     db_session.remove()
+
+@app.before_request
+def load_user_id():
+    g.user_id = session.get('user_id')
+
 
 @app.route("/")
 @app.route("/index")
@@ -31,24 +37,21 @@ def route():
 def try_tutorial(id):
 	tutorials = db_session.query(Tutorial).all()
 	tutorial = db_session.query(Tutorial).get(id)
-	
-	return render_template("try_tutorial.html", tutorial=tutorial, tutorials=tutorials)
+	rendered_tutorial = markdown.markdown(tutorial.tutorial, safe_mode='escape')
+	return render_template("try_tutorial.html", tutorial=rendered_tutorial, tutorials=tutorials)
 
-@app.route('/admin')
-def login():
-    return render_template("login.html")
 
 @app.route("/tutorials")
 def list_tutorials():
-
-	tutorials = db_session.query(Tutorial).all()
+	if g.user_id:
+		tutorials = db_session.query(Tutorial).all()
 	return render_template("list_tutorials.html", tutorials=tutorials)
 
 @app.route("/tutorial/<int:id>", methods=["GET"])
 def view_tutorial(id):
 	tutorial = db_session.query(Tutorial).get(id)
-	
-	return render_template("tutorial.html", tutorial=tutorial)
+	rendered_tutorial = markdown.markdown(tutorial.tutorial, safe_mode='escape')
+	return render_template("tutorial.html", tutorial=tutorial, tutorial_text=rendered_tutorial)
 
 @app.route("/tutorials/new_tutorial", methods=["GET", "POST"])
 def new_tutorial():
@@ -56,7 +59,7 @@ def new_tutorial():
 	form = TutorialForm(request.form)
 	if request.method == 'POST' and form.validate():
 		title = form.title.data
-		tutorial = form.text.data
+		tutorial = form.tutorial.data
 		new_tutorial = Tutorial(title=title, tutorial=tutorial)
 		db_session.add(new_tutorial)
 		db_session.commit()
@@ -74,16 +77,31 @@ def delete_tutorial(id):
 @app.route("/tutorials/edit_tutorial/<int:id>", methods=['GET', 'POST'])
 def edit_tutorial(id):
 	tutorial = db_session.query(Tutorial).get(id)
-	form = TutorialForm(request.form)
+	form = TutorialForm(request.form, tutorial)
+
 	if request.method == "POST" and form.validate():
 		tutorial.title = form.title.data
-		tutorial.tutorial = form.text.data
+		tutorial.tutorial = form.tutorial.data
 		db_session.add(tutorial)
 		db_session.commit()
 		return redirect(url_for("list_tutorials"))
-	return render_template('tutorial.html', form=form, tutorial=tutorial)
 
+	rendered_tutorial = markdown.markdown(tutorial.tutorial, safe_mode='escape')
+	return render_template('tutorial.html', form=form, tutorial=tutorial, tutorial_text=rendered_tutorial)
 
+@app.route('/admin')
+def login():
+    return render_template("login.html")
+
+@app.route("/authenticate", methods=["POST"])
+def authenticate():
+	email = request.form['email']
+	password = request.form['password']
+	user = db_session.query(User).filter_by(email=email, password=password).one()
+	session['user_id'] = user.id
+	return redirect(url_for("list_tutorials"))
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
    
 if __name__ == "__main__":
 	app.run(debug=True)
