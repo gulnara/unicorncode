20fe438421c4ff01df2d98c0f202e804e6306f95
Gulnara Mirzakarimova <gulnara@gulnara.me>
2013-05-08 18:18:56 -0700

    fixed no extra line bug in show statement

--- unicorn_tokenizer.py
+++ unicorn_tokenizer.py
@@ -19,6 +19,17 @@ class StatementToken(Token):
     def __init__(self, val):
         self.val = val
 
+class StatementList(StatementToken):
+    def __init__(self, statements):
+        self.statements = statements
+
+    def std(self):
+        pass
+
+    def eval(self):
+        for stmt in self.statements:
+            stmt.eval()
+
 class ExprToken(Token):
         pass
 
@@ -35,6 +46,7 @@ class ShowToken(StatementToken):
             # next()
             # next(NewLineToken)
         next(NewLineToken)
+        # next()
         return self
     def eval(self):
         if self.action is not None:
@@ -435,17 +447,16 @@ def unicorn_tokenize(characters):
 
 def next(expected_token_type = None):
     global token
-
     if tokens:
         if expected_token_type is not None:
-            # print "expected", type(token), expected_token_type
             if type(token) != expected_token_type:
                 raise Exception("not the expected token")
         next_t = tokens.pop(0)
         token = next_t
         return token
     else:
-        return FinalToken()
+        token = FinalToken()
+        return token
 
 def expression(rbp=0):
     global token
@@ -474,16 +485,7 @@ def statement():
         else:
             return expr
 
-class StatementList(StatementToken):
-    def __init__(self, statements):
-        self.statements = statements
 
-    def std(self):
-        pass
-
-    def eval(self):
-        for stmt in self.statements:
-            stmt.eval()
 
 def stmtlist():
     whatever = [] 
